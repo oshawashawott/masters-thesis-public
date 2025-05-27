8154b0eba55bceb69e61add3b552f6b9de18836b
oshawashawott <andrewtakesfinland@gmail.com>
2025-05-27 21:54:47 +0300

    Uploading Thesis Project Code

---
+++ Thesis_Public_Facing_Code.R
@@ -0,0 +1,20 @@
+#README
+
+
+#This is the official file containing all analytics and processing relevant to the Masters Thesis:
+#Multivariate Insights into Discrimination: Analyzing Differences in Human Values among University-Educated Young Adults
+#The sections will proceed with Data processing and description, and will be followed by the data analysis section
+#Relevant Notes will accompany each of the code sections
+
+#relevant libraries for the analysis
+library(skimr)
+library(dplyr)
+library(tidyverse)
+library(pheatmap)
+library(ggplot2)
+library(patchwork)
+library(reshape2)
+library(cluster)
+library(MASS)
+set.seed(314159)
+#Data processing
